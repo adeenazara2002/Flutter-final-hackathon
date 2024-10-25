0b0f78e688d46f1c44fa47bbc5f9147c1c5205ba
adeenazara2002 <adinazara2001@gmail.com>
2024-10-26 01:16:51 +0500

    Restaurant Screen

--- lib/Screens/Restaurant/restaurant.dart
+++ lib/Screens/Restaurant/restaurant.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class RestaurantScreen extends StatefulWidget {
   @override
@@ -18,51 +19,147 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
             fit: BoxFit.cover,
           ),
         ),
-        child: Column(
+        child: Stack(
           children: [
-            // Use Stack to place icons over the deal image
-            Stack(
-              children: [
-                // Deal image
-                Image.asset(
-                  'assets/images/deal.png',
-                  width: MediaQuery.of(context).size.width,
-                ),
-                // Icons positioned over the deal image
-                Positioned(
-                  top: 50, // Adjust the top position to your preference
-                  left: 10, // Adjust the left position to your preference
-                  right: 10, // Adjust the right position to your preference
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // Deal image
+            Image.asset(
+              'assets/images/deal.png',
+              width: MediaQuery.of(context).size.width,
+            ),
+            Positioned(
+              top: 50,
+              left: 10,
+              right: 10,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_back_ios, color: Colors.white),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  Row(
                     children: [
                       IconButton(
-                        icon: Icon(Icons.arrow_back_ios, color: Colors.white),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                      IconButton(
                         icon: Icon(Icons.favorite_border, color: Colors.white),
-                        onPressed: () {
-                        },
+                        onPressed: () {},
                       ),
                       IconButton(
                         icon: Icon(Icons.more_vert, color: Colors.white),
-                        onPressed: () {
-                        },
+                        onPressed: () {},
                       ),
                     ],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-            // Other rows or widgets below the image
-            Row(
-              children: [
-                // You can add other content here as needed
-              ],
-            )
+            // Container
+            Positioned(
+              top: 260,
+              left: 20,
+              right: 20,
+              child: Container(
+                height: 180,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.transparent,
+                ),
+                child: Column(
+                  children: [
+                    // Top part with glass effect
+                    Expanded(
+                      flex: 1,
+                      child: ClipRRect(
+                        borderRadius:
+                            BorderRadius.vertical(top: Radius.circular(10)),
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                          child: Container(
+                            color: Color.fromRGBO(20, 20, 71, 1.0)
+                                .withOpacity(0.2),
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Image.asset('assets/images/star.png'),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  'Kinka Izakaya',
+                                  style: TextStyle(
+                                    fontSize: 22.0,
+                                    fontWeight: FontWeight.w700,
+                                    color: Color.fromRGBO(255, 255, 255, 1),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    // Bottom part with dark blue color and text
+                    Container(
+                      height: 80,
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(11, 18, 37, 1),
+                        borderRadius:
+                            BorderRadius.vertical(bottom: Radius.circular(10)),
+                      ),
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  'Delievery Fee',
+                                  style: TextStyle(
+                                    color: Color.fromRGBO(255, 255, 255, 0.6),
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                             
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  '3.99',
+                                  style: TextStyle(
+                                    color: Color.fromRGBO(255, 255, 255, 1),
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                           
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: 50,
+                color: Colors.transparent,
+              ),
+            ),
           ],
         ),
       ),
